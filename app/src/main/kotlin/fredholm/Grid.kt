fd62f11f0c8ade70413e4f3d4d7d6bac32f48b00
Gleb Nasretdinov <gleb.nasretdinov@proton.me>
2025-06-08 20:53:35 +0300

    ref: add comments about fictive knots
    Signed-off-by: Gleb Nasretdinov <gleb.nasretdinov@proton.me>

--- app/src/main/kotlin/fredholm/Grid.kt
+++ app/src/main/kotlin/fredholm/Grid.kt
@@ -17,11 +17,14 @@ class Grid(a: Double, b: Double, size: Int) {
     operator fun get(i: Int): Double {
         require(i in -2..n + 2)
 
+        // There are two fictional knots on the left and two on the right, with a step size of eps from borders.
+        // The same points cannot be used because NaN is returned when evaluating 0/0 in B-splines.
+        val eps = 1e-5
         if (i == -1 || i == -2) {
-            return grid[0] + i * 1e-3
+            return grid[0] + i * eps
         }
         if (i == n + 1 || i == n + 2) {
-            return grid[n] + i * 1e-3
+            return grid[n] + i * eps
         }
         return grid[i]
     }
